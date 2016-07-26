d7ea5fca21ad69ab1063f84f1514252061d2ce57
monicafp <monikfornaris@gmail.com>
2016-07-26 19:31:01 -0400

    Add exercise 1 file

---
+++ exercise-1.js
@@ -0,0 +1,24 @@
+// This is a comment
+
+// Data types:
+
+// Primitive Data Types:
+// Number eg: 1, 2
+// String eg: "Hello", 'Hello'
+// Boolean eg: true, false
+
+// Complex Data Types:
+// Array eg: [1, "hello", false]
+// Object eg: { key : value }
+
+// Executable Data Type:
+// Function
+/*
+function hello(name) {
+  console.log("Hello From Codetrotters");
+  console.log("Nice to meet you ", name);
+}
+*/
+
+// Print to current output eg: stdin (terminal), webconsole
+console.log("Hello, world");
