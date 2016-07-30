cb6325492b30d23ced9dbabba9c356eb02829288
monicafp <monikfornaris@gmail.com>
2016-07-30 14:51:50 -0400

    Finish exercise 3

--- module-1/exercise-3.js
+++ module-1/exercise-3.js
@@ -1,13 +1,15 @@
 var prompt = require('prompt-sync')();
 
+// Usage example:
+var n = Number(prompt('How many more times?'));
+console.log(n);
+// Wrapping the prompt into a Number ensures you get the correct data type for your action (which is numerical).
+
+// Exercise
 var x = Number(prompt('What do you want as your first number?'));
 var y = Number(prompt('What do you want as your second number?'));
 console.log(x + " plus " + y + " equals ", x + y);
-
-var x = Number(prompt('What do you want as your first number?'));
-var y = Number(prompt('What do you want as your second number?'));
 console.log(x + " minus " + y + " equals ", x - y);
-
 console.log(x + " times " + y + " equals ", x * y);
 console.log(x + " divided by " + y + " equals ", x / y);
 console.log("The remainder of " + x + " divided by " + y + " is ", x % y);
