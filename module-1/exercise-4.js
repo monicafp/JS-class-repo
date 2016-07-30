1599a2a65c7cc4de1670a3cbc406dd9b6d99d658
monicafp <monikfornaris@gmail.com>
2016-07-30 15:03:53 -0400

    Finish exercise 4

--- module-1/exercise-4.js
+++ module-1/exercise-4.js
@@ -12,3 +12,21 @@ function isEven(n) {
 }
 
 isEven(n);
+
+/*
+
+Initially I wrote the following answer:
+
+    var n = Number(prompt('Which number would you like to use?'));
+
+    if (n % 2 === 0) {
+      console.log("0");
+    }
+
+    if (n % 2 !== 0) {
+      console.log("1");
+    }
+
+This worked, but later I took a closer look at the exercise's title "isEven" and decided to go with a function instead.
+
+*/
