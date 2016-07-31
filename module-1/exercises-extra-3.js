8907407da1a8d31e664c79dd5ba85fe0779b243e
monicafp <monikfornaris@gmail.com>
2016-07-30 20:04:24 -0400

    Finish extra exercise 3

--- module-1/exercises-extra-3.js
+++ module-1/exercises-extra-3.js
@@ -1,1 +1,13 @@
+var prompt = require('prompt-sync')();
 
+var n = Number(prompt("Choose a number"));
+
+var result = n / n - 1;
+
+console.log(result);
+
+if( result % 2 === 0 ) {
+  console.log("Even");
+} else {
+  console.log("Odd");
+}
