c4177b4a3c61c3f0166605c987504b4628a6b1a6
monicafp <monikfornaris@gmail.com>
2016-08-02 20:41:04 -0400

    Add and finish module 2 exercise 1

---
+++ module-2/exercise-1.js
@@ -0,0 +1,14 @@
+var prompt = require('prompt-sync')();
+
+var size = Number(prompt("Choose a size."));
+var myArray = [];
+
+function genArray(size) {
+  for (var i = 0; i < size; i++) {
+    myArray.push(0);
+  };
+  console.log(myArray.length);
+  console.log(myArray);
+}
+
+genArray(size);
