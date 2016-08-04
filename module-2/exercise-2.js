c4c3696e8aa9313facaf3a8f76bdaabf0478812c
monicafp <monikfornaris@gmail.com>
2016-08-04 19:49:54 -0400

    Review module 2 exercise 2

--- module-2/exercise-2.js
+++ module-2/exercise-2.js
@@ -2,16 +2,29 @@ var prompt = require('prompt-sync')();
 
 var n = Number(prompt("Choose a number."));
 
+// Progressive Loop
+
 function factorial(n) {
   var result = 1;
-  /*if (n == 0) {
+  if (n == 0) {
     return result;
   }
-  */
   for (var i = 1; i <= n; i++) {
     result *= i;
   }
-  console.log(result);
+  return result;
 };
 
-factorial(n);
+console.log(factorial(n));
+
+
+// Recursive
+
+function factorial(n) {
+  if (n === 1) {
+    return 1;
+  }
+  return factorial(n - 1) * n;
+}
+
+console.log(factorial(n));
