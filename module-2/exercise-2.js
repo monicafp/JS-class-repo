3cb055a4ada1f034e5fa364d2acc4eee7a11440d
monicafp <monikfornaris@gmail.com>
2016-08-04 13:36:43 -0400

    Edit module 2 exercise 2

--- module-2/exercise-2.js
+++ module-2/exercise-2.js
@@ -1,9 +1,17 @@
 var prompt = require('prompt-sync')();
 
-var n = Number(prompt("Choose a size."));
+var n = Number(prompt("Choose a number."));
 
-function fac(n) {
-  
+function factorial(n) {
+  var result = 1;
+  /*if (n == 0) {
+    return result;
+  }
+  */
+  for (var i = 1; i <= n; i++) {
+    result *= i;
+  }
+  console.log(result);
 };
 
-fac(n);
+factorial(n);
