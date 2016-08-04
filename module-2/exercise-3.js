4cd33fdd7a907cf7a594099698cd04d1839d99df
monicafp <monikfornaris@gmail.com>
2016-08-04 14:01:55 -0400

    Edit module 2 exercise 3

---
+++ module-2/exercise-3.js
@@ -0,0 +1,19 @@
+// Prompt user for String x. Print the reverse of x.
+
+var prompt = require('prompt-sync')();
+
+var x = Number(prompt("Enter any word."));
+
+// Put the given word into an array
+var x = [" "];
+// Slice the array to separate each character
+x.slice(0);
+
+// reverse array
+x.reverse();
+
+// push into new array
+var newArray = [ ];
+
+// concat new array
+newArray.concat();
