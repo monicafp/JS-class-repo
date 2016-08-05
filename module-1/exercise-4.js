3dfa52be1fc89a3fe64981bd37c37fa88e7ce0ff
monicafp <monikfornaris@gmail.com>
2016-08-04 20:05:08 -0400

    Review module 1 exercise 4

--- module-1/exercise-4.js
+++ module-1/exercise-4.js
@@ -2,6 +2,7 @@ var prompt = require('prompt-sync')();
 
 var n = Number(prompt('Which number would you like to use?'));
 
+// My answer
 function isEven(n) {
   if (n % 2 === 0) {
     console.log("0");
@@ -11,4 +12,19 @@ function isEven(n) {
   }
 }
 
-isEven(n);
+console.log(isEven(n));
+
+// Johnny's suggestion: easier to test doesn't mix execution and
+function isEven(n) {
+  if (n % 2 === 0) {
+    return true;
+  }
+  return false;
+}
+
+console.log(isEven(n));
+/*
+test("should be true with an even number") {
+  assert.equal(isEven(2), true);
+}
+*/
