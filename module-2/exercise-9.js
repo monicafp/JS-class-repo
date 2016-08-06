805604ea80a1e03d106cff20b020daa3d9dc34e0
monicafp <monikfornaris@gmail.com>
2016-08-06 19:50:41 -0400

    Edit module 2 exercise 9

--- module-2/exercise-9.js
+++ module-2/exercise-9.js
@@ -1,3 +1,16 @@
 var prompt = require('prompt-sync')();
 
 var n = Number(prompt("Enter any positive integer."));
+
+function countChar() {
+  var binary = n.toString(2);
+  var counter = 0;
+  for (var starter = 0;  starter < binary.length; starter++) {
+    if (binary.charAt(starter) === 0) {
+       counter += 1;
+    }
+  }
+  return counter;
+}
+
+console.log(countChar())
