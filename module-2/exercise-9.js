c867f26764588a7a6c3e6f84885c8854d2177ce5
monicafp <monikfornaris@gmail.com>
2016-08-08 07:29:04 -0400

    Finish module 2 exercise 9

--- module-2/exercise-9.js
+++ module-2/exercise-9.js
@@ -6,7 +6,7 @@ function countChar() {
   var binary = n.toString(2);
   var counter = 0;
   for (var starter = 0;  starter < binary.length; starter++) {
-    if (binary.charAt(starter) === 0) {
+    if (binary.charAt(starter) === "0") {
        counter += 1;
     }
   }
